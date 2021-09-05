af34c3ad4e3a1c94f9cc13676c0606ec66e912c5
aydin <aydinterzi7@gmail.com>
2021-09-05 21:56:45 +0300

    payload email eklendi

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -70,7 +70,8 @@ namespace imecappAPI.Controllers
             {
                 Subject = new ClaimsIdentity(new Claim[]{
                     new Claim(ClaimTypes.NameIdentifier,user.Id.ToString()),
-                    new Claim(ClaimTypes.Name,user.UserName)
+                    new Claim(ClaimTypes.Name,user.UserName),
+                    new Claim("email",user.Email)
                     }),
                 Expires=DateTime.UtcNow.AddDays(1),
                 SigningCredentials=new SigningCredentials(new SymmetricSecurityKey(key),SecurityAlgorithms.HmacSha256Signature)
